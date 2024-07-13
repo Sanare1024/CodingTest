9ea2f2055634abc23c11af6947b9c7f4da3170db
Sanare1024 <kjun7810@gmail.com>
2024-07-13 21:31:57 +0900

    윤년 계산 문제

---
+++ src/Bronze5/LeapYear.java
@@ -0,0 +1,19 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int year = Integer.parseInt(s);
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+    }
+}
