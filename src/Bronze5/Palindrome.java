4517682460d0f93271b67704f7391ab50c4fca7e
Sanare1024 <kjun7810@gmail.com>
2024-07-18 22:05:21 +0900

    오븐 시계 2525 팰린드롬인지 확인하기 10988

---
+++ src/Bronze5/Palindrome.java
@@ -0,0 +1,29 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine(); // word를 받아
+        String[] w = word.split(""); // 나눠서 배열에 넣어
+        int wordLength = word.length(); // 단어의 글자 수
+
+        String[] wRe = new String[wordLength]; //거꾸로 배열
+
+        for (int i = 0; i < wordLength; i++) { //단어 뒤집는중
+            wRe[i] = w[wordLength - i -1];
+        }
+
+        String wordR = String.join("",wRe); //배열 String으로 변환
+
+        int answer = 0; // 맞을경우 1로 상정하고 틀리면 0 으로 바꿔
+
+        if(word.equals(wordR)) {
+            answer = 1;
+        }
+
+        System.out.println(answer);
+    }
+}
