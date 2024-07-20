946bc84b0d66963bb1c3b64f7e47c9c93dd7c050
Sanare1024 <kjun7810@gmail.com>
2024-07-21 00:18:01 +0900

    평균 1546 숫자의 합 11720

---
+++ src/Bronze5/Sum.java
@@ -0,0 +1,21 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String a = sc.next(); //100자리수는 int나 long으로 안되니까  string으로 받
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++){
+            sum += a.charAt(i) - 48;
+        }
+
+        System.out.println(sum);
+    }
+}
