65fb8496f37079455456d453c1af39deaeea4ae6
Sanare1024 <kjun7810@gmail.com>
2024-07-23 07:32:07 +0900

    투 포인터 실전문제 백준 2018 수들의 합 5 백준 1940 주몽

--- src/Bronze5/Jumong.java
+++ src/Bronze5/Jumong.java
@@ -14,26 +14,22 @@ public class Jumong {
         int start = 0; // 스타트 인덱스
         int end = array.length - 1; // 엔드 인덱스
         int sum = 0; //인덱스 2개 합친값 넣을곳
-
         //for문으로 N개수만큼 수열 받아서 넣기
         for (int i = 0; i < array.length; i++) {
             array[i] = sc.nextInt();
         }
-
         //배열 받은거 오름차순으로 정리해야 하는데 for문 돌려야하나....?
         /*int a = 0; //임시저장용
         for (int i = 0; i < array.length; i++){
             for(int j = i + 1; j < array.length; j++){
-                if(array[i] > a[j]) {
+                if(array[i] > array[j]) {
                     a = array[i];
                     array[i] = array[j];
                     array[j] = a;
                 }
             }
         }*/
-
-        Arrays.sort(array); // 위에걸로 했는데 이렇게 좋은 기능이 있었네 ㅂㄷ
-
+        Arrays.sort(array); // 위에가 혼자 짠건데 이렇게 좋은 기능이 있었네 ㅂㄷ
         while (start < end) { //투 포인터
             sum = array[start] + array[end];
             if (sum < m) {
@@ -45,9 +41,7 @@ public class Jumong {
             } else {
                 end--;
             }
-
         }
-
         System.out.println(count);
     }
 }
