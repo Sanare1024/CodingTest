afa785685a1c5be6a9fe17d17effc19a528f3a6a
Ku Hyun Jun <kjun7810@gmail.com>
2024-08-27 19:09:28 +0900

    그리디 알고리즘 연습 문제
    그리디 알고리즘에 대한 이해와 해결능력,구현 구현능력 향상을 위한 문제
    
    1339 단어 수학

--- src/Greedy/Bk1339.java
+++ src/Greedy/Bk1339.java
@@ -1,9 +1,8 @@
-package G4tr;
+package Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Bk1339 {
