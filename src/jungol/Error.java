6fbb862d8b7b5f7374f65ea0ee0f06034534a60b
Ku Hyun Jun <kjun7810@gmail.com>
2024-09-27 22:36:25 +0900

    구현 연습 문제 구현에 대한 이해와 해결능력, 구현능력 향상을 위한 문제
    정올 오류수정

---
+++ src/jungol/Error.java
@@ -0,0 +1,59 @@
+package jungol;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Error {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = 0;
+        N=Integer.parseInt(br.readLine());
+
+        int[][] arr= new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] row = new int[N];
+        int[] col = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(arr[i][j]==1)
+                    col[i]++;
+                if(arr[j][i]==1)
+                    row[i]++;
+            }
+        }
+        int rowCnt=0;
+        int rowCheck=0;
+        int colCnt=0;
+        int colCheck=0;
+        for (int i = 0; i < N; i++) {
+            if(col[i]%2!=0) {
+                colCnt++;
+                colCheck=i+1;
+            }
+            if(row[i]%2!=0) {
+                rowCnt++;
+                rowCheck=i+1;
+            }
+        }
+        System.out.println(Arrays.toString(row));
+        System.out.println(Arrays.toString(col));
+        if(rowCnt==0 && colCnt==0)
+            System.out.println("OK");
+        else if(rowCnt==1 && colCnt==1) {//유일하게 해결가능할때
+            System.out.printf("Change bit (%d,%d)\n",colCheck,rowCheck);
+        }
+        else System.out.println("Corrupt");
+
+    }
+}
