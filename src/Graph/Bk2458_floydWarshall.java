e8d7a15fd0a00c8ffb083b936d12b9fdb2e0d47f
Ku Hyun Jun <kjun7810@gmail.com>
2024-10-15 00:48:28 +0900

    플로이드워셜(그래프 알고리즘) 연습 문제
    플로이드워셜 알고리즘에 대한 이해와 해결능력, 구현능력 향상을 위한 문제
    2458 키 순서

---
+++ src/Graph/Bk2458_floydWarshall.java
@@ -0,0 +1,46 @@
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bk2458_floydWarshall {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[][] possible =  new boolean[n + 1][n + 1];
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            possible[a][b] = true;
+        }
+
+        //플로우드워셜 경출도
+        for (int k = 1; k < n + 1; k++){
+            for (int i = 1; i < n + 1; i ++){
+                for (int j = 1; j < n + 1; j++){
+                    if(possible[i][k] && possible[k][j]){
+                        possible[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i < n + 1; i++){
+            int count = 0;
+            for (int j = 1; j < n + 1; j++){
+                if(possible[i][j] || possible[j][i]) count++;
+            }
+
+            if(count == n - 1) ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
