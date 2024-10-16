239605d7044d37834e72996b993cc0ec527fa709
Ku Hyun Jun <kjun7810@gmail.com>
2024-10-16 18:47:51 +0900

    구현 연습 문제
    구현에 대한 이해와 해결능력, 구현능력 향상을 위한 문제
    중간값 찾기

---
+++ src/jungol/middleValue.java
@@ -0,0 +1,30 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class middleValue {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int T=sc.nextInt();
+        for(int test_case=1;test_case<=1;test_case++) {
+            //System.out.println("#"+test_case+" ");
+            int N=sc.nextInt();
+            int[] arr=new int[101];
+
+            for (int i = 0; i < N; i++) {
+                arr[sc.nextInt()]++;
+            }
+            int max=0;
+            int index=0;
+            for (int i = 0; i < N; i++) {
+                max+=arr[i];//빈도를 더해서
+                if(max> N/2) {//중앙넘는 순간
+                    index=i;
+                    break;
+                }
+            }
+            System.out.println(index);
+        }
+    }
+}
