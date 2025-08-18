7e9c91d3a953e36fa600e518df00e02612c2201d
Ku Hyun Jun <kjun7810@gmail.com>
2025-08-18 23:00:46 +0900

    [CodingTest][BaekJoon][2025-08-18]feat: 뱀과 사다리 게임

---
+++ src/bfs/Bk16928.java
@@ -0,0 +1,58 @@
+package bfs;
+
+import java.io.*;
+import java.util.*;
+
+public class Bk16928 {
+    static int n, m;
+    static int[] arr;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[101];
+        visit = new boolean[101];
+
+        for (int i = 0; i < n + m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[x] = y;
+        }
+
+        bfs(1);
+    }
+
+    static void bfs(int start) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start, 0});
+        visit[start] = true;
+
+        while (!q.isEmpty()) {
+            int p[] = q.poll();
+
+            if (p[0] == 100) {
+                System.out.println(p[1]);
+                return;
+            }
+
+            for (int i = 1; i < 7; i++) {
+                int next = p[0] + i;
+                if (next <= 100) {
+                    if (arr[next] != 0) {
+                        next = arr[next];
+                    }
+                    if (!visit[next]) {
+                        visit[next] = true;
+                        q.add(new int[]{next, p[1] + 1});
+
+                    }
+                }
+            }
+        }
+    }
+}
