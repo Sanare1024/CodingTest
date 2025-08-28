a3de158d7d39e88a189bd86e5f66c853b74814e9
Ku Hyun Jun <kjun7810@gmail.com>
2025-08-28 22:18:22 +0900

    [CodingTest][BaekJoon][2025-08-28]feat: 1, 2, 3 더하기

--- src/dp/Bk9095.java
+++ src/dp/Bk9095.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class Bk9095 {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         int[] dp = new int[11];
@@ -13,10 +13,11 @@ public class Bk9095 {
         dp[2] = 2;
         dp[3] = 4;
 
-        for(int i=4; i<11; i++) {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-        for(int i=0; i<t; i++) {
+
+        for (int i = 0; i < t; i++) {
             int num = Integer.parseInt(br.readLine());
             System.out.println(dp[num]);
         }
