05467e6ea1fb9e3307786736e27f8d99931c8284
Ku Hyun Jun <kjun7810@gmail.com>
2025-08-28 22:17:54 +0900

    [CodingTest][BaekJoon][2025-08-28]feat: 1, 2, 3 더하기

--- src/dp/Bk9095.java
+++ src/dp/Bk9095.java
@@ -1,2 +1,24 @@
-package dp;public class Bk9095 {
+package dp;
+
+import java.util.*;
+import java.io.*;
+
+public class Bk9095 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i=4; i<11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        for(int i=0; i<t; i++) {
+            int num = Integer.parseInt(br.readLine());
+            System.out.println(dp[num]);
+        }
+    }
 }
