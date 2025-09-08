453bfe9c9f3b5adffc902e84f53e14e0ee74d0c6
Ku Hyun Jun <kjun7810@gmail.com>
2025-09-09 00:07:52 +0900

    [CodingTest][BaekJoon][2025-09-08]feat: 색종이 만들기

--- src/recursion/Bk2630.java
+++ src/recursion/Bk2630.java
@@ -1,2 +1,74 @@
-package recursion;public class Bk2630 {
+package recursion;
+
+import java.util.*;
+import java.io.*;
+
+public class Bk2630 {
+
+    public static int white = 0;
+    public static int blue = 0;
+    public static int[][] board;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        board = new int[n][n];
+
+        StringTokenizer st;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        partition(0, 0, n);
+
+        System.out.println(white);
+        System.out.println(blue);
+
+    }
+
+    public static void partition(int row, int col, int size) {
+
+        if (colorCheck(row, col, size)) {
+            if (board[row][col] == 0) {
+                white++;
+            } else {
+                blue++;
+            }
+            return;
+        }
+
+        int newSize = size / 2;  //반절
+
+        // 재귀
+        partition(row, col, newSize);                        // 2사분면
+        partition(row, col + newSize, newSize);                // 1사분면
+        partition(row + newSize, col, newSize);                // 3사분면
+        partition(row + newSize, col + newSize, newSize);    // 4사분면
+    }
+
+
+    // 현재 파티션의 컬러가 같은지 체크한다.
+    public static boolean colorCheck(int row, int col, int size) {
+
+        int color = board[row][col];    // 첫 번째 원소를 기준으로 검사
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                // 색상이 같이 않다면 false를 리턴
+                if (board[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+        // 검사가 모두 통과했다는 의미이므로 true 리턴
+        return true;
+    }
 }
