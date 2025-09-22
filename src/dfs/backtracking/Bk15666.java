e102e10418d85c13fb59ebcbd8b04bd79bd68aa5
Ku Hyun Jun <kjun7810@gmail.com>
2025-09-22 23:59:25 +0900

    [CodingTest][BaekJoon][2025-09-22]feat: N과 M (12)

---
+++ src/dfs/backtracking/Bk15666.java
@@ -0,0 +1,52 @@
+package dfs.backtracking;
+
+import java.util.*;
+
+public class Bk15666 {
+    static int n, m;
+    static int[] map;
+    static int[] result;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        map = new int[n];
+        result = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            map[i] = sc.nextInt();
+        }
+        Arrays.sort(map);
+
+        StringBuilder sb = new StringBuilder();
+
+        cycle(0, 0, sb);
+
+        System.out.print(sb);
+
+    }
+
+    private static void cycle(int start, int cnt, StringBuilder sb) {
+
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+
+        } else {
+
+            int num = 0;
+            for (int i = start; i < n; i++) {
+                if (num == map[i]) continue;
+
+                result[cnt] = map[i];
+                cycle(i, cnt + 1, sb);
+                num = map[i];
+            }
+        }
+    }
+}
